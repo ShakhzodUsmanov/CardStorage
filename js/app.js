e2ef9ac967fefc4cb8f650faf8a6c81fb4d1e8a3
Shakhzod Usmanov <usmanovshaxzod11@gmail.com>
2024-07-18 17:32:33 +0500

    Login checker created

--- js/app.js
+++ js/app.js
@@ -1,49 +1,58 @@
-const loginForm = document.getElementById('loginForm')
-const registerForm = document.getElementById('registerForm')
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
 
-const api = "https://crudcrud.com/api/1e77f61792b242789197805dacb2e171"
+const api = "https://crudcrud.com/api/1e77f61792b242789197805dacb2e171";
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-loginForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
-
-  const formData = new FormData(loginForm)
-
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
-  });
-
-  console.log(data);
-
-  loginForm.reset()
-})
-
-registerForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
-
-  const formData = new FormData(registerForm)
+  const formData = new FormData(loginForm);
 
   const data = {};
   formData.forEach((value, key) => {
     data[key] = value;
   });
 
+  fetchUsers(data);
+  loginForm.reset();
+});
 
-  CreateUser(data)
-  registerForm.reset()
-})
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const formData = new FormData(registerForm);
+
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  CreateUser(data);
+  registerForm.reset();
+});
 
 async function CreateUser(userData) {
-  userData.cart = []
-  console.log( userData);
+  userData.cart = [];
+  console.log(userData);
 
   await fetch(`${api}/users`, {
-    method: "POST", 
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(userData)
-  })
+    body: JSON.stringify(userData),
+  });
+}
+
+async function fetchUsers(user) {
+  const data = await fetch(`${api}/users`);
+  const usersData = await data.json();
+
+  usersData.forEach((elem) => {
+    if (elem.email == user.email) {
+      console.log(user.email, "correct!");
+    } else {
+      console.log("user email is wrong");
+    }
+  });
 }
